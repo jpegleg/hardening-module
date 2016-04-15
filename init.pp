3e1862e7c6347dec957f0f3d4abfb4da590f8d6b
Keegan Bowen <keegan.bowen@gmail.com>
2016-04-14 23:58:16 -0600

    Update init.pp

--- init.pp
+++ init.pp
@@ -1,5 +1,5 @@
 class hardening-module {
-  file { '/usr/local/bin/iptlm',
+  file { '/usr/local/bin/iptlm':
       ensure => file,
       source => 'puppet:///modules/hardening-module/iptables-lockdown-mode',
       owner  => 'root',
@@ -7,18 +7,20 @@ class hardening-module {
       mode   => '0755',
     }
 case $operatingsystem {
-  'RedHat', 'CentOS', 'Fedora': { '/etc/yum.conf',
+  'RedHat', 'CentOS', 'Fedora': {
+  file { '/etc/yum.conf':
       ensure => file,
       owner  => 'root',
       group  => 'root',
       mode   => '0644',
-  file { '/usr/local/bin/hardening-module',
+  }
+  file { '/usr/local/bin/hardening-module':
       ensure => file,
       source => 'puppet:///modules/hardening-module/hardening-module-centos',
       owner  => 'root',
       mode   => '0644',
     }
-  file { '/usr/local/bin/exportenv',
+  file { '/usr/local/bin/exportenv':
       ensure => file,
       source => 'puppet:///modules/hardening-module/iptables-lockdown-mode',
       owner  => 'root',
@@ -26,196 +28,198 @@ case $operatingsystem {
       mode   => '0755',
     }
   }
-
-  'Debian', 'Ubuntu', 'Kubuntu', 'Trisquel': { '/etc/apt/sources.list',
+  'Debian', 'Ubuntu', 'Kubuntu', 'Trisquel': {
+  file { '/etc/apt/sources.list':
       ensure => file,
       owner  => 'root',
       group  => 'root',
       mode   => '0644',
-  file { '/usr/local/bin/exportenv',
+  }
+  file { '/usr/local/bin/exportenv':
       ensure => file,
       content=> 'export DEB_BUILD_HARDENING=1',
       owner  => root,
       group  => root,
       mode   => '0755',
     }
-  file { '/usr/local/bin/exportenv2',
+  file { '/usr/local/bin/exportenv2':
       ensure => file,
       source => 'puppet:///modules/hardening-module/iptables-lockdown-mode',
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  }
-
-  default: { '/usr/local/bin/exportenv',
+  
+  default: { 
+  file { '/usr/local/bin/exportenv':
       ensure => file,
       source => 'puppet:///modules/hardening-module/iptables-lockdown-mode',
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
-  }
-  default: {  '/usr/local/bin/hardening-module',
+    }
+  file { '/usr/local/bin/hardening-module':
       ensure => file,
       source => 'puppet:///modules/hardening-module/hardening-module-default',
       owner  => 'root',
       mode   => '0644',
+    }
   }
-
-  file { '/etc/passwd',
+  }
+  file { '/etc/passwd':
       ensure => file,
       owner  => 'root',
       group  => 'root',
       mode   => '0644',
     }
-  file { '/etc/sudoers',
+  file { '/etc/sudoers':
       ensure => file,
       owner  => 'root',
       group  => 'root',
       mode   => '0440',
     }
-  file { '/etc/shadow',
+  file { '/etc/shadow':
       ensure => file,
       owner  => 'root',
       group  => 'root',
       mode   => '0640',
     }
-  file { '/etc/group',
+  file { '/etc/group':
       ensure => file,
       owner  => 'root',
       group  => 'root',
       mode   => '0640',
     }
-
-  file { '/home',
+  file { '/home':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/etc',
+  file { '/etc:
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/root',
+  file { '/root':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0700',
     }
-  file { '/etc/systemd',
+  file { '/etc/systemd':
       ensure => directory,
       owner  => 'root',
       group  => 'root'
       mode   => '755',
-  file { '/lib',
+    }
+  file { '/lib':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/lib32',
+  file { '/lib32':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/lib64',
+  file { '/lib64':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/boot',
+  file { '/boot':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/proc',
+  file { '/proc':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0555',
     }
-  file { '/opt',
+  file { '/opt':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/dev',
+  file { '/dev':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/bin',
+  file { '/bin':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/var',
+  file { '/var':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/mnt',
+  file { '/mnt':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/lost+found',
+  file { '/lost+found':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0700',
     }
-  file { '/sys',
+  file { '/sys':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/tmp',
+  file { '/tmp':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0776',
     }
-  file { '/run',
+  file { '/run':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/sbin',
+  file { '/sbin':
       ensure => directory,
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  file { '/usr/local/bin/exportlimits',
+  file { '/usr/local/bin/exportlimits':
       ensure => file,
       source => 'puppet:///modules/hardening-module/limits',
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
     }
-  exec { 'exportlimits',
+  exec { 'exportlimits':
       command=> 'exportlimits',
       path   => '/usr/local/bin/exportlimits',
     }
-  exec { 'exportenv2',
+  exec { 'exportenv2':
       command=> 'exportenv2 22',
       path   => '/usr/local/bin/exportenv2',
     }
-  exec { 'hardening-module',
+  exec { 'hardening-module':
       command=> 'hardening-module',
       path   => '/usr/local/bin/hardening-module',
     }
